2783f6a8fdd7df911ca908da60068b23f191ddc7
shintanabilla23 <nabillashinta168@gmail.com>
2020-10-04 12:39:03 +0700

    Add project files.

---
+++ CurrencyApps/CurrencyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CurrencyApps
+{
+    class CurrencyController
+    {
+        public string usdToIdr(string nominal)
+        {
+            var nominalDouble = Convert.ToDouble(nominal);
+            var result = nominalDouble * 15000;
+            return Convert.ToString(result);
+        }
+
+        public Boolean isAllowed(string nominal)
+        {
+            Regex regex = new Regex("[^0-9.-]+");
+            return !regex.IsMatch(nominal);
+        }
+
+    }
+}
